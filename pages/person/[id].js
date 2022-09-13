815e4c46533945bfff787bc259bf8e35a8bc60d3
1403253225 <1403253225@qq.com>
2022-09-13 18:04:48 +0800

    add tailwind

--- pages/person/[id].js
+++ pages/person/[id].js
@@ -4,7 +4,7 @@ import {useRouter} from 'next/router'
 export default function Home() {
     const { query } = useRouter()
   return (
-    <div >
+    <div className="flex">
         <h1 >
           Welcome People {query.id}
         </h1>
